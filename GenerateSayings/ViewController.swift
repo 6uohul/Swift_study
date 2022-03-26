88d784a16ea413247bab0270cde3db943a5c5424
6uohul <kiy010604@naver.com>
2022-03-26 21:23:59 +0900

    Initial Commit

---
+++ GenerateSayings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenerateSayings
+//
+//  Created by 김인영 on 2022/03/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
