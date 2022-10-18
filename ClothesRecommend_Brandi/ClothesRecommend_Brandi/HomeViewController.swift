45e1b148d74af5970e4ab093130a3ed1b8f25618
6uohul <kiy010604@naver.com>
2022-10-19 02:27:47 +0900

    #1 브랜디 프로젝트 생성

---
+++ ClothesRecommend_Brandi/ClothesRecommend_Brandi/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  ClothesRecommend_Brandi
+//
+//  Created by 김인영 on 2022/10/19.
+
+//  홈 화면
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
