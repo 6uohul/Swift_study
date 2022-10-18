15790f4cd080ed8dda6b835fc2d58f48f23a12bc
6uohul <kiy010604@naver.com>
2022-10-19 04:24:53 +0900

    [#1] CollectionView를 이용한 간단한 UI 구현

--- ClothesRecommend_Brandi/ClothesRecommend_Brandi/ClothesCollectionViewCell.swift
+++ ClothesRecommend_Brandi/ClothesRecommend_Brandi/ClothesCollectionViewCell.swift
@@ -6,7 +6,88 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class ClothesCollectionViewCell: UICollectionViewCell {
+//MARK: - ClothesCollectionViewCell
+
+final class ClothesCollectionViewCell: UICollectionViewCell {
     
+    // MARK: - identifier
+    static let identifier = "ClothesCollectionViewCell"
+    
+    // MARK: - UI Components
+    private let clothesContainerView = UIView()
+    private let clothesImageView = UIImageView()
+    private let titleLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 10, weight: .thin)
+    }
+    private let brandLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+    private let priceLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+    }
+    
+    // MARK: - Life Cycles
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+extension ClothesCollectionViewCell {
+    
+    //MARK: - Layout
+    private func layout() {
+        backgroundColor = .clear
+        contentView.backgroundColor = .clear
+        [clothesContainerView, titleLabel, brandLabel, priceLabel].forEach {
+            contentView.addSubview($0)
+        }
+        clothesContainerView.addSubview(clothesImageView)
+        clothesContainerView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(8)
+            $0.centerX.equalToSuperview()
+            $0.leading.equalToSuperview().offset(9)
+            $0.height.equalTo(190)
+        }
+        
+        clothesImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        brandLabel.snp.makeConstraints {
+            $0.top.equalTo(clothesContainerView.snp.bottom).offset(8)
+            $0.leading.equalTo(clothesContainerView).offset(4)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(brandLabel.snp.bottom).offset(4)
+            $0.leading.equalTo(brandLabel)
+        }
+        
+        priceLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(6)
+            $0.leading.equalTo(titleLabel)
+        }
+        
+    }
+    
+    //MARK: - dataBind
+    
+    func dataBind(model: ClothesModel) {
+        titleLabel.text = model.title
+        brandLabel.text = model.brand
+        priceLabel.text = model.price
+        clothesImageView.image = UIImage(named: model.clothesImage)
+    }
 }
--- ClothesRecommend_Brandi/ClothesRecommend_Brandi/HomeViewController.swift
+++ ClothesRecommend_Brandi/ClothesRecommend_Brandi/HomeViewController.swift
@@ -22,7 +22,8 @@ class HomeViewController: UIViewController {
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.isScrollEnabled = true
         collectionView.showsVerticalScrollIndicator = true
-        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         return collectionView
     }()
     
@@ -30,21 +31,24 @@ class HomeViewController: UIViewController {
     var ClothesList : [ClothesModel] = [
         ClothesModel(clothesImage: "clothes_1", title: "썸머부클꽈배기배색가디건", brand: "알로항", price: "29,900원"),
         ClothesModel(clothesImage: "clothes_2", title: "멜로 크롭 니트 - 4 color", brand: "메이글", price: "20,000원"),
-        ClothesModel(clothesImage: "clothes_3", title: "킬러 카고 와이드 데님 팬츠", brand: "완스몰", price: "32,900원")
+        ClothesModel(clothesImage: "clothes_3", title: "킬러 카고 와이드 데님 팬츠", brand: "완스몰", price: "32,900원"),
+        ClothesModel(clothesImage: "clothes_4", title: "폴로랄프로렌 V넥 꽈배기 니트", brand: "폴로랄프로렌", price: "219,000원"),
+        ClothesModel(clothesImage: "clothes_5", title: "플라워 퍼프 랩 원피스", brand: "어썸", price: "67,000원"),
+        ClothesModel(clothesImage: "clothes_6", title: "캘빈클라인 로고 반팔 티셔츠", brand: "캘빈클라인", price: "95,700원")
     ]
     
     //MARK: - Constants
-    final let ClothesInset: UIEdgeInsets = UIEdgeInsets(top: 55, left: 20, bottom: 10, right: 20)
-    final let ClothesLineSpacing: CGFloat = 10
-    final let ClothesInterItemSpacing: CGFloat = 21
-    final let ClothesCellHeight: CGFloat = 200
+    final let ClothesInset: UIEdgeInsets = UIEdgeInsets(top: 30, left: 15, bottom: 10, right: 15)
+    final let ClothesLineSpacing: CGFloat = 70
+    final let ClothesInterItemSpacing: CGFloat = 10
+    final let ClothesCellHeight: CGFloat = 360
     
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         layout()
+        register()
     }
-    
     
 
 }
@@ -52,6 +56,8 @@ class HomeViewController: UIViewController {
 // MARK: - Extensions
 
 extension HomeViewController {
+    
+    //MARK: - layout
     private func layout() {
         view.backgroundColor = .white
         view.addSubview(clothesCollectionView)
@@ -60,14 +66,62 @@ extension HomeViewController {
             $0.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
             $0.bottom.equalToSuperview()
             $0.height.equalTo(calculateCellHeight())
-            
         }
-    
     }
     
     func calculateCellHeight() -> CGFloat {
         let count = CGFloat(ClothesList.count)
         let heightCount = count / 2 + count.truncatingRemainder(dividingBy: 2)
         return heightCount * ClothesCellHeight + (heightCount - 1) * ClothesLineSpacing + ClothesInset.top + ClothesInset.bottom
+    }
+    
+    //MARK: - register
+    private func register() {
+        clothesCollectionView.register(ClothesCollectionViewCell.self, forCellWithReuseIdentifier: ClothesCollectionViewCell.identifier)
+    }
+}
+
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+
+        // cell 하나의 크기를 정해주는 함수에요. CGSize를 리턴합니다.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let screenWidth = UIScreen.main.bounds.width
+        let doubleCellWidth = screenWidth - ClothesInset.left - ClothesInset.right - ClothesInterItemSpacing
+        return CGSize(width: doubleCellWidth / 2, height: 198)
+    }
+    
+        // Layout 구현 흐름에서 줄 바꿈의 간격을 정해주는 함수에요.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return ClothesLineSpacing
+    }
+    
+        // Layout 구현 흐름에서 줄 바꿈이 아닌 셀 간 간격을 정해주는 함수에요.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return ClothesInterItemSpacing
+    }
+    
+        // CollectionView 섹션 하나 당의 Inset을 정해주는 함수에요.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return ClothesInset
+    }
+}
+
+// MARK: -UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+        
+        // cell 몇 개 만들래? 라고 물어봅니다,,,
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ClothesList.count
+    }
+    
+        // 어떤 cell을 만들거니? 라고 물어보네요,,,
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let clothesCell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ClothesCollectionViewCell.identifier, for: indexPath)
+                as? ClothesCollectionViewCell else { return UICollectionViewCell() }
+        clothesCell.dataBind(model: ClothesList[indexPath.item])
+        return clothesCell
     }
 }
