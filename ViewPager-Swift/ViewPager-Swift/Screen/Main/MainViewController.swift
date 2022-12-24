9f642fadeca0da7f687209862561116c627ff7f1
6uohul <kiy010604@naver.com>
2022-12-24 17:28:38 +0900

    [#3] ViewPager - 폴더링

---
+++ ViewPager-Swift/ViewPager-Swift/Screen/Main/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  ViewPager-Swift
+//
+//  Created by 김인영 on 2022/12/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
